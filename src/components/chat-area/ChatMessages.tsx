b4638288c4f51f8045835d04b577bb853bbc47e7
Igor-Sasic <igor.sasic811@gmail.com>
2026-01-27 15:10:40 +0100

    switching to regular div instead of scrollarea

--- src/components/chat-area/ChatMessages.tsx
+++ src/components/chat-area/ChatMessages.tsx
@@ -1,23 +1,32 @@
 import { useParams } from "@tanstack/react-router";
 import useMessageQuery from "./useMessageQuery";
-import { ScrollArea } from "../ui/scroll-area";
 import MessageBubble from "./MessageBubble";
+import { useEffect, useRef } from "react";
 
 const ChatMessages = () => {
   const { chatId } = useParams({ from: "/_protected/app/chat/$chatId" });
   const { chatMessages } = useMessageQuery(chatId);
 
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (scrollRef.current)
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+  }, [chatMessages.length]);
+
   return (
-    <div className="flex-1 overflow-y-hidden p-2">
-      <ScrollArea className="h-full w-full">
-        {chatMessages.map((msg) => {
-          return (
-            <MessageBubble key={msg.id} msg={msg}/>
-          );
-        })}
-      </ScrollArea>
+    <div className="flex flex-col h-full overflow-y-auto" ref={scrollRef}>
+      <div className="grow"></div>
+      <div className="flex flex-col-reverse gap-1">
+        {!chatMessages.length && (
+          <p>No messages yet. Be the first, send something</p>
+        )}
+        {chatMessages.length > 0 &&
+          chatMessages.map((msg) => {
+            return <MessageBubble key={msg.id} msg={msg} />;
+          })}
+      </div>
     </div>
   );
 };
-
 export default ChatMessages;
